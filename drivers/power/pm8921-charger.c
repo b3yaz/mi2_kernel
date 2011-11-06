6eef80c4968a8ba23e727de48aaff2431218d793
Ezekeel <notezekeel@googlemail.com>
2011-11-06 19:28:49 +0100

    Added Battery Life eXtender (BLX) version 1.

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -27,9 +27,14 @@
 #include <linux/workqueue.h>
 #include <linux/debugfs.h>
 #include <linux/slab.h>
+#include <linux/blx.h>
 
 #include <mach/msm_xo.h>
 #include <mach/msm_hsusb.h>
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
 
 #define CHG_BUCK_CLOCK_CTRL	0x14
 
@@ -1518,7 +1523,7 @@ static int get_prop_batt_status(struct pm8921_chg_chip *chip)
 	int i;
 
 	if (chip->ext_psy) {
-		if (chip->ext_charge_done)
+		if (get_prop_batt_capacity(chip) > get_charginglimit())
 			return POWER_SUPPLY_STATUS_FULL;
 		if (chip->ext_charging)
 			return POWER_SUPPLY_STATUS_CHARGING;
@@ -1675,7 +1680,40 @@ static void __pm8921_charger_vbus_draw(unsigned int mA)
 			i--;
 		if (i < 0)
 			i = 0;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge == 1)
+			i = 14;
+		else if (force_fast_charge == 2) {
+			switch (fast_charge_level) {
+				case FAST_CHARGE_500:
+					i = 2;
+					break;
+				case FAST_CHARGE_700:
+					i = 4;
+					break;
+				case FAST_CHARGE_900:
+					i = 8;
+					break;
+				case FAST_CHARGE_1100:
+					i = 10;
+					break;
+				case FAST_CHARGE_1300:
+					i = 12;
+					break;
+				case FAST_CHARGE_1500:
+					i = 14;
+					break;
+				default:
+					break;
+			}
+		}
 		rc = pm_chg_iusbmax_set(the_chip, i);
+		pr_info("charge curent index => %d\n", i);
+#else
+		rc = pm_chg_iusbmax_set(the_chip, i);
+#endif
+
 		if (rc) {
 			pr_err("unable to set iusb to %d rc = %d\n", i, rc);
 		}
@@ -3114,7 +3152,7 @@ static int is_charging_finished(struct pm8921_chg_chip *chip)
  * - this happens when the battery is removed or EOC has
  * happened
  */
-#define CONSECUTIVE_COUNT	3
+#define CONSECUTIVE_COUNT	4
 static void eoc_worker(struct work_struct *work)
 {
 	struct delayed_work *dwork = to_delayed_work(work);
@@ -3124,7 +3162,11 @@ static void eoc_worker(struct work_struct *work)
 	int end;
 
 	pm_chg_failed_clear(chip, 1);
-	end = is_charging_finished(chip);
+
+	if(get_prop_batt_capacity(chip) > get_charginglimit())
+		end = CHG_FINISHED;
+	else
+		end = is_charging_finished(chip);
 
 	if (end == CHG_NOT_IN_PROGRESS) {
 		count = 0;
@@ -3138,7 +3180,7 @@ static void eoc_worker(struct work_struct *work)
 		count = 0;
 	}
 
-	if (count == CONSECUTIVE_COUNT) {
+	if (count == CONSECUTIVE_COUNT && get_prop_batt_capacity(chip) > get_charginglimit()) {
 		count = 0;
 		pr_info("End of Charging\n");
 
