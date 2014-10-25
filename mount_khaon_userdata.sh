0a609ac16aafa31ae16c641b395862853efd03ae
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2014-10-25 18:11:47 +0200

    userdata mounting scripts: mounting script added for TDB
    For now the kernel doesn't determine itself if the user has enabled TDB. Therefore users will have to pick the correct version accordingly

--- mount_khaon_userdata.sh
+++ mount_khaon_userdata.sh
@@ -5,16 +5,6 @@ MOUNT_POINT=$2
 LOG_FILE="/dev/null"
 LOG_LOCATION="/data/.fsck_log/"
 
-# get syspart-flag from cmdline
-set -- $(cat /proc/cmdline)
-for x in "$@"; do
-    case "$x" in
-        syspart=*)
-        SYSPART=$(echo "${x#syspart=}")
-        ;;
-    esac
-done
-
 # storage log
 if [ "${MOUNT_POINT}" == "/storage_int" ]; then
     mkdir ${LOG_LOCATION}
@@ -23,46 +13,15 @@ if [ "${MOUNT_POINT}" == "/storage_int" ]; then
     LOG_FILE=${LOG_LOCATION}/storage_${TIMESTAMP}.log
 fi
 
-DATA=`blkid /dev/block/mmcblk0p26 | grep "f2fs"`;
+DATA=`/sbin/blkid /dev/block/mmcblk0p26 | grep "f2fs"`
 
-#USERDATA FS TYPE
-if [ "${DATA}" != "" ]; then
-	FS_TYPE=f2fs
-else
-	FS_TYPE=ext4
-fi;
-
-#USERDATA OPTS
-if [ "${DATA}" != "" ]; then
-	OPTS=rw,noatime,nosuid,nodev,discard,nodiratime,inline_xattr,inline_data,flush_merge
-else
-	OPTS=noatime,nosuid,nodev,barrier=1,noauto_da_alloc
-fi;
-
-# mount userdata partition
+# mount partition
 if [ -e ${BLOCK_DEVICE} ]; then
-    # make /data_root
-    mkdir -p /data_root
-    chmod 0755 /data_root
-    mount -t ${FS_TYPE} -o ${OPTS} ${BLOCK_DEVICE} /data_root
-
-    if [ "${SYSPART}" == "system" ];then
-        BINDMOUNT_PATH="/data_root/system0"
-    elif [ "${SYSPART}" == "system1" ];then
-        BINDMOUNT_PATH="/data_root/system1"
-    else
-        reboot recovery
-    fi
-    if [ -e /data_root/.truedualboot ];then
-
-        # bind mount
-        mkdir -p ${BINDMOUNT_PATH}
-        chmod 0755 ${BINDMOUNT_PATH}
-        mount -o bind ${BINDMOUNT_PATH} ${MOUNT_POINT}
-    else
-        umount /data_root
-        mount -t ${FS_TYPE} -o ${OPTS} ${BLOCK_DEVICE} ${MOUNT_POINT}
-    fi
+	if [ "${DATA}" != "" ]; then
+		 mount -t f2fs -o rw,noatime,nosuid,nodev,discard,nodiratime,inline_xattr,inline_data,flush_merge ${BLOCK_DEVICE} ${MOUNT_POINT}
+	else
+		 mount -t ext4 -o noatime,nosuid,nodev,barrier=1,noauto_da_alloc ${BLOCK_DEVICE} ${MOUNT_POINT}
+	fi;
 
 fi
 
